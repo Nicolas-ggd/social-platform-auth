5fd7fd2ae427b0ea6a4973840194c27a19c17a0d
Nicolas-ggd <nikusha.gogitidze.03@gmail.com>
2023-08-06 00:34:40 +0400

    update

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -23,5 +23,5 @@ export default NextAuth({
       clientSecret: process.env.DISCORD_CLIENT_SECRET || "",
     }),
   ],
-  secret: process.env.NEXT_PUBLIC_SECRET
+  secret: process.env.NEXTAUTH_SECRET
 });
