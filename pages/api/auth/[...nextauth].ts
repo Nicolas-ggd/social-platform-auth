d8536ee05a1c2540c3a6231f4f00af3bcd8c9337
Nicolas-ggd <nikusha.gogitidze.03@gmail.com>
2023-08-06 00:03:49 +0400

    update

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -8,21 +8,20 @@ export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     GoogleProvider({
-      clientId:
-        "62830888335-lj6pudf0jpj4us2en9e080ch84m46h75.apps.googleusercontent.com",
-      clientSecret: "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",
+      clientId: process.env.GOOGLE_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
     }),
     GithubProvider({
-      clientId: "eff93604d5c94b7e2590",
-      clientSecret: "a0b5e51d415b8469e47c18c1bba2ebb5f9876365",
+      clientId: process.env.GITHUB_ID || "",
+      clientSecret: process.env.GITHUB_CLIENT_SECRET || "",
     }),
     FacebookProvider({
-      clientId: "638586751571937",
-      clientSecret: "acc7f3972f1134c843655559860c5e09",
+      clientId: process.env.FACEBOOK_ID || "",
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET || "",
     }),
     DiscordProvider({
-      clientId: "1137464676582707321",
-      clientSecret: "wGGNKCEOhz-T4YHssjyh-zZ5UADU8KKM",
+      clientId: process.env.DISCORD_ID || "",
+      clientSecret: process.env.DISCORD_CLIENT_SECRET || "",
     }),
     // ...add more providers here
   ],
